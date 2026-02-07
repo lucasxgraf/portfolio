8c59ceeca7354a68978f76dcb92a5299019c5edc
Lucas Graf <lucasgraf01@gmail.com>
2026-02-07 15:34:28 +0100

    feat: add main hero content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from "./features/loader-component/loader-component";
+import { HeroComponent } from './features/portfolio-component/hero-component/hero-component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
-    LoaderComponent],
+    LoaderComponent,
+    HeroComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
