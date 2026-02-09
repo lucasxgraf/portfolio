b1f1589c970a939d5909fcc2dc799d34bdcb32d3
Lucas Graf <lucasgraf01@gmail.com>
2026-02-09 17:32:09 +0100

    feat: add marquee component + optimized content layout header, hero content

--- src/app/features/portfolio/hero/hero.component.ts
+++ src/app/features/portfolio/hero/hero.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { Header } from '../../../core/components/header/header';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [TranslateModule,
+    Header
+  ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  marqueeItems:string[] = [
+    'HERO.MARQUEE.REMOTE_WORK',
+    'HERO.MARQUEE.JOB_TITLE',
+    'HERO.MARQUEE.LOCATION',
+    'HERO.MARQUEE.OPEN_TO_WORK'
+  ];
 }
--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { Header } from "../../core/components/header/header";
 import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [Header, 
-    HeroComponent],
+  imports: [HeroComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
