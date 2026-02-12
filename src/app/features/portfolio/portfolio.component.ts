febf747ba926925a70d757099dd291e4a0e5a198
Lucas Graf <lucasgraf01@gmail.com>
2026-02-12 12:56:37 +0100

    feat: implement skills section with dynamic data, images

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [HeroComponent, AboutMeComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
--- src/app/features/portfolio/skills/skills.component.ts
+++ src/app/features/portfolio/skills/skills.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  skills = [
+    { name: 'Angular', icon: 'assets/img/skills/frontend/angular.png' },
+    { name: 'TypeScript', icon: 'assets/img/skills/frontend/typeScript.png' },
+    { name: 'JavaScript', icon: 'assets/img/skills/frontend/javaScript.png' },
+    { name: 'HTML', icon: 'assets/img/skills/frontend/html.png' },
+    { name: 'CSS', icon: 'assets/img/skills/frontend/css.png' },
+    { name: 'Firebase', icon: 'assets/img/skills/tools/firebase.png' },
+    { name: 'Git', icon: 'assets/img/skills/tools/git.png' },
+    { name: 'REST-API', icon: 'assets/img/skills/tools/rest-api.png' },
+    { name: 'Material Design', icon: 'assets/img/skills/frontend/materialDesign.png' },
+    { name: 'Scrum', icon: 'assets/img/skills/tools/scrum.png' },
+    { name: 'Growth mindset', icon: 'assets/img/skills/tools/growthMindset.png' }
+  ];
 }
