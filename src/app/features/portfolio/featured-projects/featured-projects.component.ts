63dfb41859ff15f797b23820474cdb1112b5e2e2
Lucas Graf <lucasgraf01@gmail.com>
2026-02-12 15:02:56 +0100

    feat: Add FeaturedProjects component and integrate it into the portfolio page.

---
+++ src/app/features/portfolio/featured-projects/featured-projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-projects',
+  imports: [],
+  templateUrl: './featured-projects.component.html',
+  styleUrl: './featured-projects.component.scss',
+})
+export class FeaturedProjects {
+
+}
--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { FeaturedProjects } from './featured-projects/featured-projects.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [HeroComponent, AboutMeComponent, SkillsComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, FeaturedProjects],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
