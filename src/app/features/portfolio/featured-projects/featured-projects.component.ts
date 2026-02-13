6de6e30a20c8407fd6bad5aea5c4eb37b1b9ea70
Lucas Graf <lucasgraf01@gmail.com>
2026-02-13 09:39:00 +0100

    feat: implement featured projects section with dynamic list rendering, hover effects, and a new outward arrow icon.

--- src/app/features/portfolio/featured-projects/featured-projects.component.ts
+++ src/app/features/portfolio/featured-projects/featured-projects.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
+export interface Project {
+  name: string;
+  technologies: string[];
+  previewImage: string;
+  githubLink: string;
+  liveTestLink: string;
+}
+
 @Component({
   selector: 'app-featured-projects',
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './featured-projects.component.html',
   styleUrl: './featured-projects.component.scss',
 })
 export class FeaturedProjects {
-
+  hoveredIndex: number | null = null;
+  
+  projects: Project[] = [
+    {
+      name: 'Pokedex',
+      technologies: ['JavaScript', 'RestAPI', 'HTML', 'CSS'],
+      previewImage: 'Comming soon',
+      githubLink: 'Comming soon',
+      liveTestLink: 'Comming soon',
+    },
+    {
+      name: 'Join',
+      technologies: ['JavaScript', 'Firebase', 'HTML', 'CSS'],
+      previewImage: 'Comming soon',
+      githubLink: 'Comming soon',
+      liveTestLink: 'Comming soon',
+    },
+    {
+      name: 'El Pollo Loco',
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+      previewImage: 'Comming soon',
+      githubLink: 'Comming soon',
+      liveTestLink: 'Comming soon',
+    },
+  ];
 }
