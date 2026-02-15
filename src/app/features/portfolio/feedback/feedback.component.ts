82e083d432a36c2803eceb2031f9af6072c314cd
Lucas Graf <lucasgraf01@gmail.com>
2026-02-15 15:44:55 +0100

    feat: Implement a new feedback section with i18n support and a card-based display.

--- src/app/features/portfolio/feedback/feedback.component.ts
+++ src/app/features/portfolio/feedback/feedback.component.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-colleagues',
+  selector: 'app-feedback',
   standalone: true,
   imports: [CommonModule, TranslateModule],
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.scss']
 })
 export class FeedbackComponent {
-  
+  feedbacks = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 }
+  ];
 }
