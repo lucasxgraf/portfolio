feec24311f3896d2bf5f6dca04017ad6e27702ef
Lucas Graf <lucasgraf01@gmail.com>
2026-02-16 11:44:59 +0100

    feat: Add contact form component with internationalization and assets.

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -4,10 +4,11 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { FeaturedProjects } from './featured-projects/featured-projects.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [HeroComponent, AboutMeComponent, SkillsComponent, FeaturedProjects, FeedbackComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, FeaturedProjects, FeedbackComponent, ContactFormComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
