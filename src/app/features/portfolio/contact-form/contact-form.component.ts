100d278da073dd3dbfb64dc7dbdf6f0f4befb827
Lucas Graf <lucasgraf01@gmail.com>
2026-02-17 14:17:42 +0100

    feat: Add hover effects to the header logo and privacy policy link, and update the contact form's privacy policy layout and routing

--- src/app/features/portfolio/contact-form/contact-form.component.ts
+++ src/app/features/portfolio/contact-form/contact-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [TranslateModule],
+  imports: [TranslateModule,
+    RouterLink
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss',
 })
