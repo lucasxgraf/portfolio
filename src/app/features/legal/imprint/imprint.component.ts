179576f7dcb1dd4247c37338271111b45cad6e9e
Lucas Graf <lucasgraf01@gmail.com>
2026-02-18 13:51:40 +0100

    feat: Add imprint page with internationalized content and dedicated styling

--- src/app/features/legal/imprint/imprint.component.ts
+++ src/app/features/legal/imprint/imprint.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
